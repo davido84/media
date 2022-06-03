804a1f6b3180ff0ab010aecf5b977cf883413421
David Ossorio <davidossorio@gmail.com>
2022-06-02 22:32:34 -0700

    Clean up

--- validate_video.py
+++ validate_video.py
@@ -16,11 +16,12 @@ logger = logging.getLogger('VALIDATE')
 @dataclass
 class Result:
     skipped: int = 0
-    added: int = 0
+    removed: int = 0
     errors: int = 0
     timeouts: int = 0
     validator_failed_errors: int = 0
     total_mkv_bytes: int = 0
+    total_titles_validated: int = 0
     total_mkv_bytes_human: str = ''
 
 
@@ -43,16 +44,21 @@ def command(vm: VideoManager, temp_folder: str):
             click.secho('Finished with TIMEOUT', fg='bright_yellow')
             break
 
+        if not Path(iso_file).exists():
+            logger.info(f'Removed non-existent file: {iso_file!s}')
+            result.removed += 1
+            del iso_dict[iso_file]
+            continue
+
         if not vm.force and iso_dict[iso_file].validated:
             result.skipped += 1
             continue  # Already processed
 
-        iso_dict[iso_file].validated = True
         message = f'{iso_file!s} ({iso_count+1} of {len(iso_dict.items())})'
         click.secho(message, fg='cyan')
         for title_count, title in enumerate(dict_info.titles):
             output_file = Path(temp_folder, dict_info.titles[title][mkvcodes.output_file])
-            # click.secho(f'  {output_file}')
+
             run_makemkvcon(f'{output_file!s} ({title_count+1} of {len(dict_info.titles)})',
                            ['mkv', f'iso:{iso_file}', f'{title}', temp_folder], timeout=60*30, show_progress=True)
             if not output_file.exists():
@@ -60,14 +66,17 @@ def command(vm: VideoManager, temp_folder: str):
                 logging.error(f'{iso_file!s}, {output_file!s}: NO_OUTPUT_FILE')
                 result.errors += 1
                 continue
+
             click.secho(f'Validating {output_file!s} ... ', nl=False, fg='yellow')
+
             try:
                 subprocess.run([shutil.which('mkvalidator.exe'), '--quiet', '--quick', f'{output_file!s}'],
                                text=True, capture_output=True, check=True)
                 click.secho('OK', fg='bright_green')
-                result.added += 1
                 result.total_mkv_bytes += output_file.stat().st_size
                 iso_dict[iso_file].valid_titles.add(title_count)
+                result.total_titles_validated += 1
+
             except subprocess.CalledProcessError:
                 click.secho(f'FAILED', fg='bright_red')
                 logging.error(f'{iso_file!s}, {output_file!s}: FAILED validation.')
@@ -75,6 +84,8 @@ def command(vm: VideoManager, temp_folder: str):
 
             finally:
                 output_file.unlink(missing_ok=True)
+
+        iso_dict[iso_file].validated = True
 
     vm.save_yaml_dict(iso_dict)
     vm.save_iso_dict(iso_dict)
