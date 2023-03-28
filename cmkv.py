00874ed1cb72bff4e93f8483bc7a8d8377c1f4af
David Ossorio <davidossorio@gmail.com>
2023-03-27 22:02:04 -0700

    Added logging

--- cmkv.py
+++ cmkv.py
@@ -3,13 +3,15 @@ from pathlib import Path
 import os
 from media_util import gigabyte_string
 import subprocess
+import logging
 
 def run_mkv(iso_file: Path, program_args) -> bool:
 
     mkv_args = ['makemkvcon.exe', '--messages=-null',  '--noscan', '--cache=2000', '--minlength=180',
                 'mkv', f'iso:{str(iso_file)}', 'all', f'{str(program_args.output_path)}']
 
-    print(f'Running makemkvcon.exe: {mkv_args}')
+    cmd = ''.join([f'"{S}" ' for S in mkv_args]).strip()
+    logging.getLogger().info(f'Running makemkvcon.exe: {cmd}')
 
     result = 0
 
@@ -25,8 +27,8 @@ def process_iso(file: Path, program_args) -> bool:
     os.makedirs(str(program_args.output_path), exist_ok=True)
     result = run_mkv(file, program_args)
 
-    iso_file_size = file.stat().st_size
     if result and not program_args.dry_run:
+        iso_file_size = file.stat().st_size
         output_files = [F for F in program_args.output_path.glob('*.mkv')]
         output_files = sorted(output_files, key =  lambda x: os.stat(str(x)).st_size)
         total_bytes = folder_mkv_size(program_args.output_path)
@@ -36,12 +38,23 @@ def process_iso(file: Path, program_args) -> bool:
                 total_bytes = folder_mkv_size(program_args.output_path)
             assert output_files
 
+        file_sizes: set[int] = set()
+        for file in output_files:
+            file_size = file.stat().st_size
+            if file_size in file_sizes:
+                file.unlink()
+            else:
+                file_sizes.add(file_size)
+
     return result
 
 
 def convert_mkv(program_args):
-    print(f'Dry run: {program_args.dry_run}')
-    print(f'Limit: {program_args.limit}')
+    logger = logging.getLogger()
+    logger.info('Starting MKV conversion.')
+
+    logger.info(f'Dry run: {program_args.dry_run}')
+    logger.info(f'Limit: {program_args.limit}')
     total_mkv_bytes = 0
     total_iso_bytes = 0
     has_errors = False
@@ -49,7 +62,7 @@ def convert_mkv(program_args):
     program_args.input_path = Path(str(program_args.search_folder))
     for file in program_args.input_path.rglob('*.iso'):
         file_size = file.stat().st_size
-        print(f'Processing ISO: {file.name}, ({gigabyte_string(file_size)})')
+        logger.info(f'Processing ISO: "{str(file)}", ({gigabyte_string(file_size)})')
         media_stem = list(file.parts[len(program_args.input_path.parts):])
         media_stem[-1] = file.stem
         final_output_path = str(program_args.output if program_args.output else program_args.input)
@@ -57,7 +70,7 @@ def convert_mkv(program_args):
 
         result = process_iso(file, program_args)
         if not result:
-            print(f'ERROR')
+            logger.critical(f'ERROR')
             has_errors = True
             break
 
@@ -67,19 +80,39 @@ def convert_mkv(program_args):
         total_iso_bytes += file_size
         total_mkv_bytes += folder_mkv_size(program_args.output_path)
 
-    print(f'Total ISO processed: {gigabyte_string(total_iso_bytes)}')
-    print(f'Total MKV processed: {gigabyte_string(total_mkv_bytes)}')
+    logger.info(f'Total ISO processed: {gigabyte_string(total_iso_bytes)}')
+    logger.info(f'Total MKV processed: {gigabyte_string(total_mkv_bytes)}')
 
     if has_errors:
-        print('Finished with errors.')
+        logger.info('Finished with errors.')
     else:
-        print('Finished! No errors.')
+        logger.info('Finished! No errors.')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert mkv files')
     parser.add_argument('-d', '--dry-run', action='store_true', default=False, help='Dry run')
     parser.add_argument('-m', '--limit', type=int, help='Processing limit in gigabytes.', default=1000)
-    parser.add_argument('-o', '--output', type=str, help='Output folder. If specified, input ISO files will not be deleted')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Output folder. If specified, input ISO files will not be deleted')
     parser.add_argument('search_folder', type=str, help='Search folder')
     args = parser.parse_args()
+
+    log_file_name = Path(args.search_folder, 'mkv-log.txt')
+
+    logging.basicConfig(format='%(levelname)s: %(message)s',
+                        filename=str(log_file_name),
+                        encoding='utf-8',
+                        filemode='w',
+                        level=logging.DEBUG)
+
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger().addHandler(console)
+
     convert_mkv(args)
