2c8974911a88184f73e13efc188e92e3a884819b
David Ossorio <davidossorio@gmail.com>
2023-04-03 21:25:42 -0700

    Removed dry run

--- cmkv.py
+++ cmkv.py
@@ -22,11 +22,7 @@ def _run_mkv(iso_file: Path, output_path: Path, program_args) ->None:
     cmd = ''.join([f'"{S}" ' for S in mkv_args]).strip()
     logging.debug(f'{cmd}')
 
-    if program_args.dry_run:
-        output_file = Path(program_args.output_path, 'title.mkv')
-        logging.debug(f'Output file: str{output_file}')
-    else:
-        subprocess.run(mkv_args, check=True, capture_output=True)
+    subprocess.run(mkv_args, check=True, capture_output=True)
 
 def _process_iso(input_file: Path, program_args) ->None:
     media_stem = [L.strip() for L in list(input_file.parts[len(program_args.search_folder.parts):])]
@@ -38,7 +34,6 @@ def _process_iso(input_file: Path, program_args) ->None:
 
         logging.debug(f'Output path: {str(output_path)}')
         os.makedirs(str(output_path), exist_ok=True)
-
         _run_mkv(input_file, output_path, program_args)
         iso_file_size = input_file.stat().st_size
 
@@ -48,7 +43,7 @@ def _process_iso(input_file: Path, program_args) ->None:
         while mkv_files and _total_mkv_size(output_path) > iso_file_size:
             file_to_remove: Path = mkv_files.pop()
             logging.debug(
-                f'Removing title: {str(file_to_remove)} - {gigabyte_string(file_to_remove.stat().st_size)}')
+                f'[R] Removing PLAY ALL title: {str(file_to_remove)} - {gigabyte_string(file_to_remove.stat().st_size)}')
             file_to_remove.unlink()
 
         # Remove any duplicate files
@@ -56,38 +51,37 @@ def _process_iso(input_file: Path, program_args) ->None:
         for iso_file in _mkv_files(output_path):
             file_size = iso_file.stat().st_size
             if file_size in file_size_set:
-                logging.debug(f'Removing duplicate title: {str(iso_file)}')
+                logging.debug(f'[D] Removing DUPLICATE title: {str(iso_file)}')
                 iso_file.unlink()
             else:
                 file_size_set.add(file_size)
 
         program_args.total_iso_bytes += iso_file_size
         program_args.total_mkv_bytes += _total_mkv_size(output_path)
+    else:
+        logging.debug(f'Skipping existing output.')
 
 def convert_mkv(program_args):
     logging.info('Starting MKV conversion.')
 
-    logging.info(f'Dry run: {program_args.dry_run}')
     logging.info(f'Limit: {gigabyte_string(program_args.limit)}') # limit bytes to process in GB
 
     for counter, iso_file in enumerate(Path(program_args.search_folder).rglob('*.iso')):
         iso_file_size = iso_file.stat().st_size
-        logging.debug(f'{_MESSAGE_HEADER} "{str(iso_file)}" ; {counter} {_MESSAGE_HEADER}')
+        logging.debug(f'{_MESSAGE_HEADER} "{str(iso_file)}" : {counter} {_MESSAGE_HEADER}')
         logging.info(f'"{str(iso_file)}", ({gigabyte_string(iso_file_size)})')
         _process_iso(iso_file, program_args)
 
         if program_args.total_iso_bytes > program_args.limit:
-            logging.info(f'Exit after processing {gigabyte_string(program_args.total_iso_bytes)}')
+            logging.info(f'Exiting after processing {gigabyte_string(program_args.total_iso_bytes)}')
             break
 
         if program_args.delete:
-            if not program_args.dry_run:
-                iso_file.unlink()
+            iso_file.unlink()
             logging.debug(f'Removed file: {iso_file}')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract mkv files.')
-    parser.add_argument('-y', '--dry-run', action='store_true', default=False, help='Dry run')
     parser.add_argument('-m', '--limit', type=int, help='Processing limit in gigabytes.', default=100)
     parser.add_argument('-d', '--delete', help='Delete input .iso file.', action='store_true', default=False)
     parser.add_argument('-f', '--force', help='Overwrite output files', action='store_true', default=False)
@@ -126,11 +120,10 @@ if __name__ == "__main__":
             convert_mkv(args)
             logging.debug(f'{_MESSAGE_HEADER} Remove Empty Folders {_MESSAGE_HEADER}')
             remove_empty_folders(str(args.search_folder))
-
             logging.debug(f'{_MESSAGE_HEADER} Summary {_MESSAGE_HEADER}')
-            logging.info(f'ISO data processed: {gigabyte_string(args.total_iso_bytes)}')
-            logging.info(f'MKV data processed: {gigabyte_string(args.total_mkv_bytes)}')
-            logging.info(f'Delta: {gigabyte_string(args.total_iso_bytes - args.total_mkv_bytes)}')
+            logging.info(f'ISO bytes processed: {gigabyte_string(args.total_iso_bytes)}')
+            logging.info(f'MKV bytes processed: {gigabyte_string(args.total_mkv_bytes)}')
+            logging.info(f'Reduction: {gigabyte_string(args.total_iso_bytes - args.total_mkv_bytes)}')
             logging.info('Finished!')
     except IOError as e:
         logging.critical(f'IO Error: {str(e)}')
