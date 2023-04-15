ebcb5190a7f0b841f34f2a85101fa9dc72e1f2ca
David Ossorio <davidossorio@gmail.com>
2023-04-15 04:46:00 -0700

    Fix check only

--- cv.py
+++ cv.py
@@ -1,21 +1,34 @@
 import argparse
-import logging
+import sys
 from pathlib import Path
 from mkvlib import mkv_info
 from media_util import setup_logging
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert Video Files')
+def main():
+    parser = argparse.ArgumentParser(description='Convert Video File Tools')
     parser.add_argument('-i', '--input', type=str, help='Input folder or file')
     parser.add_argument('-o', '--output', type=str, help='Output folder')
-    parser.add_argument('-g', '--logfile', type=str, help='Log file name', default=None)
+    parser.add_argument('--log', action='store_true', default=False, help='Create log file')
+    parser.add_argument('--log-name', type=str, help='Logfile name', default='cv.log')
 
-    subparsers = parser.add_subparsers(title='Commands', description='Convert Video Commands')
+    subparsers = parser.add_subparsers(title='Commands', description='Commands')
+
+    # mkv-info Command
     parser_filename = subparsers.add_parser('mkv-info', description='Run mkv info', aliases=['mi'])
-
+    parser_filename.add_argument('--check', action='store_true', default=False,
+                                 help='Check that mke-info can run without errors.')
     parser_filename.set_defaults(func=mkv_info)
+
     args = parser.parse_args()
-    setup_logging(args.logfile)
+
+    if args.input is None:
+        print('Missing --input')
+        return -1
+
+    setup_logging(str(Path(args.input,args.log_name)) if args.log else None)
 
     args.func(args)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
--- mkvlib.py
+++ mkvlib.py
@@ -1,25 +1,32 @@
 import logging
 from pathlib import Path
 import subprocess
-import re
 
-_ENCODING = 'ISO-8859-1'
+# _ENCODING = 'ISO-8859-1'
 
 def mkv_info(program_args):
-    if program_args.input is None:
-        print('Missing input/output paths.')
-        return
-
-    logging.info('MKV info')
+    # logging.info('MKV info')
     input_path = Path(program_args.input)
-    logging.info(f'Input path: {str(input_path)}')
+    # logging.info(f'Input path: {str(input_path)}')
+    error_files = []
 
-    for iso_file in Path(program_args.input).rglob('*.iso'):
-        logging.info(str(iso_file))
+    for iso_file in input_path.rglob('*.iso'):
+        print(f'{str(iso_file)}...', end='')
         output_file = iso_file.with_suffix('.info')
         output = subprocess.run(['makemkvcon64.exe', '--noscan', '-r', '--cache=2000', '--minlength=0',
                                  'info', str(iso_file)], check=True, capture_output=True)
-        # file_lines = output.stdout.decode('utf-8').split('\r\n')
-        # output_file.write_text('\n'.join([L for L in file_lines if re.match(r'^[TCS]INFO', L)]), encoding='utf-8')
+        try:
+            output.stdout.decode('utf-8')
+            print('OK')
+        except UnicodeDecodeError:
+            error_files.append(iso_file)
+            print('ERROR')
 
-    logging.info('Finished!')
+        if not program_args.check:
+            pass
+            # output_file.write_text('\n'.join([L for L in file_lines if re.match(r'^[TCS]INFO', L)]), encoding='utf-8')
+
+    print(f'Finished - {len(error_files)} error(s).')
+
+    for file in error_files:
+        logging.error(str(file))
